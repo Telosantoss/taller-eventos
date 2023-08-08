3c6b66a18fadd28c9caf92e0d9be202c950e8b90
Axel Santos <telobrum80@gmail.com>
2023-08-08 16:31:12 -0300

    agregando div con boton y estilos

--- eventos.js
+++ eventos.js
@@ -1,3 +1,12 @@
 document.getElementById("saludarButton").onclick = function() {
     alert("Hola!");
 };
+
+document.addEventListener("DOMContentLoaded", function() {
+    var modificarboton = document.getElementById("modificarboton");
+    modificarboton.addEventListener("click", function() {
+        alert("Hola! Soy el div");
+    });
+});
+
+
